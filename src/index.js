d19f9da81e177b00cf5dc721331573f80a5e0134
chennan <chennan_cn11@163.com>
2021-03-26 13:51:56 +0800

    add React & css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+ReactDOM.render(
+    <div className="content">HelloWorld</div>,
+    document.getElementById('root')
+)
