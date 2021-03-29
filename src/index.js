c077326849df3b595cbf23db9466951204974bdd
chennan <chennan_cn11@163.com>
2021-03-29 09:37:09 +0800

    add sass config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import './index.less'
+import './index.scss'
 
 ReactDOM.render(
     <div className="content1">Hello
-        <span className="content">China</span>
+        <span className="content-sass">China!</span>
     </div>,
     document.getElementById('root')
 )
