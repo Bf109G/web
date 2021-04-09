5dde4d40de77456bc24be8be8adca0e9754d36c4
chennan <chennan_cn11@163.com>
2021-04-09 13:46:20 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    // "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "class-methods-use-this": 0,
+    eqeqeq: 1,
+    "prefer-const": 1,
+    "no-nested-ternary": 1,
+    "lines-between-class-members": 2,
+    "no-shadow": 1,
+    "no-empty-function": 1,
+    "no-param-reassign": 1,
+    "no-underscore-dangle": 1,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "react/jsx-uses-react": 0,
+    "react/react-in-jsx-scope": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'class-methods-use-this': 0,
-        'eqeqeq': 1,
-        'prefer-const': 1,
-        'no-nested-ternary': 1,
-        'lines-between-class-members': 2,
-        'no-shadow': 1,
-        'no-empty-function': 1,
-        'no-param-reassign': 1,
-        'no-underscore-dangle': 1,
-        '@typescript-eslint/explicit-module-boundary-types': 0
-    },
-    "settings":{
-        "react":{
-            "version" : 'detect'
-        }
-    }
+  },
 };
