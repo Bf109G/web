fe90b38ecb4a3acaefeae112657dfdcb2ee6205e
chennan <chennan_cn11@163.com>
2021-05-10 16:38:38 +0800

    router

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { MenusLayout } from "@/layouts/MenusLayout";
+import { Provider } from "mobx-react";
+import stores from "./store";
 
 export class AppRouter extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={MenusLayout} />
-        </Switch>
-      </BrowserRouter>
+      <Provider {...stores}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={MenusLayout} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
