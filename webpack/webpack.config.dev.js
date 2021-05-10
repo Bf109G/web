fc08e16bc9f6d5af6f76b0f1af9e38dfec2376a9
chennan <chennan_cn11@163.com>
2021-05-10 16:43:09 +0800

    webpack.config.dev

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -3,7 +3,7 @@ const baseWebpackConfig = require("./webpack.config.base");
 
 module.exports = (env) => {
   return webpackMerge.merge(baseWebpackConfig, {
-    mode: process.env.NODE_ENV || 'development',
+    mode: process.env.NODE_ENV || "development",
     plugins: [],
     devServer: {
       historyApiFallback: true,
