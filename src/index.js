3524cfe299c8c21f474ecddf19a106c1586cad41
chennan <chennan_cn11@163.com>
2021-05-10 16:39:07 +0800

    入口index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import "./index.less";
 import "./index.scss";
 import { AppRouter } from "@/router";
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+function bootstrap() {
+  render(<AppRouter />, document.getElementById("root"));
+}
+
+bootstrap();
